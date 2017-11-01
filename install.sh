afe12255ec824e294f916864d439d1783748419b
reeoss <reeoss@gmail.com>
2017-11-02 01:52:30 +0900

    fixed, env bug

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NVIM_DIR="$HOME/.config/nvim"
+export NVIM_DIR="$HOME/.config/nvim"
 ln -s "`pwd`/nvim/init.vim" "$NVIM_DIR/init.vim"
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
